836e5af5003dbbd0384063516aa51f2438058432
somyote4 <somyote.4@gmail.com>
2019-07-06 12:54:58 +0700

    ver0.7

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY=os.environ['SECRET_KEY']
+#SECRET_KEY=os.environ['SECRET_KEY']
 DB_USERNAME=os.environ['DB_USERNAME']
 DB_PASSWORD=os.environ['DB_PASSWORD']
 DB_HOST=os.environ['DB_HOST']
