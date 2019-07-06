f299b8c19ca3cc13a1b43c1380f425b6d0f30320
somyote4 <somyote.4@gmail.com>
2019-07-06 15:38:15 +0700

    ver0.74

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 import os
 
 SECRET_KEY='you-will-never-guess' #os.environ['SECRET_KEY']
-DB_USERNAME=os.environ['DB_USERNAME']
-DB_PASSWORD=os.environ['DB_PASSWORD']
-DB_HOST=os.environ['DB_HOST']
+DB_USERNAME='sa' #os.environ['DB_USERNAME']
+DB_PASSWORD='T@st1234'  #os.environ['DB_PASSWORD'] 
+DB_HOST='192.168.17.89'#os.environ['DB_HOST'] #T@st1234
 DATABASE_NAME='ref_web'  
 #DB_URI = "postgresql://%s:%s@%s:5432/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
 DB_URI = "mssql+pymssql://sa:T@st1234@192.168.17.89/ref_web"
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 #MYSQL_ROOT_PASSWORD=os.environ['MYSQL_ROOT_PASSWORD']
-BLOG_NAME=os.environ['BLOG_NAME']
-BLOG_POST_IMAGES_PATH=os.environ['BLOG_POST_IMAGES_PATH']
+BLOG_NAME='Reference Data' #os.environ['BLOG_NAME']
+BLOG_POST_IMAGES_PATH='../static/images/uploads'#'D:/Python/Code/flogger-master/static/images/uploads' #os.environ['BLOG_POST_IMAGES_PATH']
 #DB_URI = "mysql+pymysql://%s:%s@%s:3306/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
