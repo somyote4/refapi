44dd98fef5c6e41bad0cd29251d5d5409c1ab451
somyote4 <somyote.4@gmail.com>
2019-07-06 14:13:29 +0700

    ver0.72

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
 
-#SECRET_KEY=os.environ['SECRET_KEY']
+SECRET_KEY='you-will-never-guess' #os.environ['SECRET_KEY']
 DB_USERNAME=os.environ['DB_USERNAME']
 DB_PASSWORD=os.environ['DB_PASSWORD']
 DB_HOST=os.environ['DB_HOST']
-DATABASE_NAME=os.environ['DATABASE_NAME']
+DATABASE_NAME='ref_web'  #os.environ['DATABASE_NAME']
 #DB_URI = "postgresql://%s:%s@%s:5432/%s" % (DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
 DB_URI = "mssql+pymssql://sa:T@st1234@192.168.17.89/ref_web"
 SQLALCHEMY_DATABASE_URI = DB_URI
